e4a9ba06d6e6f969a88b5ae3d9d12bb97f6e3eac
David Pires <developer.david@outlook.com>
2023-11-14 01:10:02 -0300

    Resolução de exercício UriOnlineJudge_1010

---
+++ src/main/_14_UriOnlineJudge_1010.java
@@ -0,0 +1,42 @@
+package main;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class _14_UriOnlineJudge_1010 {
+
+	public static void main(String[] args) {
+		// https://www.beecrowd.com.br/judge/pt/problems/view/1010
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int codigoPeca1, qtdPeca1;
+		double valorUnitario1,valorTotal1 ;
+		
+		codigoPeca1 = sc.nextInt();
+		qtdPeca1 = sc.nextInt();
+		valorUnitario1 = sc.nextDouble();
+	
+		valorTotal1 = (double) qtdPeca1 * valorUnitario1;
+		
+		
+		
+		int codigoPeca2, qtdPeca2;
+		double valorUnitario2, valorTotal2;
+		
+		codigoPeca2 = sc.nextInt();
+		qtdPeca2 = sc.nextInt();
+		valorUnitario2 = sc.nextDouble();
+
+		valorTotal2 = (double) qtdPeca2 * valorUnitario2;	
+		
+		Double somaDasDuasLinhas = valorTotal1 + valorTotal2;
+		
+		System.out.printf("VALOR A PAGARA: R$ %.2f%n" , somaDasDuasLinhas );
+		
+		sc.close();
+
+	}
+
+}
