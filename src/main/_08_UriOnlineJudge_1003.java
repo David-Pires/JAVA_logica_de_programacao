f89d7383342b7bed308fdf6a9aac99e7bee958e5
David Pires <developer.david@outlook.com>
2023-11-13 22:17:08 -0300

    Resolução de exercicio UriOnlineJudge_1003

---
+++ src/main/_08_UriOnlineJudge_1003.java
@@ -0,0 +1,21 @@
+package main;
+
+import java.util.Scanner;
+
+public class _08_UriOnlineJudge_1003 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int A, B, SOMA;
+		
+		A = sc.nextInt();
+		B = sc.nextInt();
+		SOMA = A + B;
+		
+		System.out.println("SOMA = " + SOMA);
+		
+		sc.close();
+	}
+
+}
