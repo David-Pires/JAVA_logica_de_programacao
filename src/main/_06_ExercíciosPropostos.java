5eaa6ac235f7bebfbb99281afc9b4cbfae1aed99
David Pires <developer.david@outlook.com>
2023-11-13 22:09:59 -0300

    Resolução de exercicio UriOnlineJudge_1002

---
+++ src/main/_06_ExercíciosPropostos.java
@@ -0,0 +1,21 @@
+package main;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class _06_ExercíciosPropostos {
+
+	public static void main(String[] args) {
+		// https://www.beecrowd.com.br/judge/pt/problems/view/1002
+		
+		
+	}
+
+}
+
+/*
+
+1002,1003,1004, 1007, 1008, 1010, 1014, 1016,1017
+1005, 1006 - média ponderada
+
+*/
---
+++ src/main/_07_UriOnlineJudge_1002.java
@@ -0,0 +1,10 @@
+package main;
+
+public class _07_UriOnlineJudge_1002 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
