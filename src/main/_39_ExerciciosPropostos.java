7f7e8c4f07131d44d531ddde801918198400fd86
David Pires <developer.david@outlook.com>
2023-12-04 11:49:46 -0300

    Resolução parcial de exercicio UriOnlineJudge_1037

--- src/main/_39_ExerciciosPropostos.java
+++ src/main/_39_ExerciciosPropostos.java
@@ -6,7 +6,7 @@ public class _39_ExerciciosPropostos {
 		// 
 		/* Exercícios propostos do URI Online Judge:
 		 * 
-		 * Primeiro grupo: 1035, 1038, 1044, 1048;
+		 * Primeiro grupo: 1035*, 1038, 1044, 1048;
 		 * Segundo grupo: 1037, 1040(utilizar float), 1041, 1047, 1049, 1051;
 		 * 
 		 * Exercícios suplementares:
